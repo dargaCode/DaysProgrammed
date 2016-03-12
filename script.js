39cfda11a73a08ddce742a69fa51258c6e47b71b
dargaCode <darga.code@gmail.com>
2016-03-11 16:15:46 -0800

    Add javascript to update the hero h1

---
+++ script.js
@@ -0,0 +1,19 @@
+var START = new Date("May 10 2015");
+var TODAY = new Date();
+var MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function getNthDay(endDate) {
+  var msElapsed = endDate - START;
+  var daysElapsed = msElapsed / MS_PER_DAY;
+  var roundedDays = Math.floor(daysElapsed);
+  var nthDay = roundedDays + 1;
+
+  return nthDay;
+}
+
+function updateHeroH1(nthDay) {
+  var heroH1 = document.querySelector(".hero h1");
+  heroH1.innerText = "DAY " + nthDay;
+}
+
+updateHeroH1(getNthDay(TODAY));
